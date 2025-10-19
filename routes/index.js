45f9c64fefe8d2b99a95ca6c7a6be384a3688e6f
Eldar <lopio.lololozka@gmail.com>
2025-10-19 16:06:31 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/strateg', function(req, res, next) {
+    res.send("<h1>Страгическая авиация</h1>")
+});
+router.get('/civil', function(req, res, next) {
+    res.send("<h1>Гражданская авиация</h1>")
+});
+router.get('/private', function(req, res, next) {
+    res.send("<h1>Частная авиация</h1>")
+});
 module.exports = router;
