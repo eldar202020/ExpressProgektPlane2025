d29814ec154f0222a2267ef781406950a206df17
Eldar Odzha <lopio.lololozka@gmail.com>
2025-11-20 12:07:51 +0300

    plane_7_1

---
+++ testMongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
+
+const Plane = mongoose.model('plane', { name: String });
+
+var planes = new Plane({ name: 'privat' });
+planes.save().then(() => console.log('fly'));
