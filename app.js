41790bf0aa1fe6fc8957f8226aee9081ceedc7d0
Eldar Odzha <lopio.lololozka@gmail.com>
2025-11-30 16:55:48 +0300

    plane_8_2

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var indexPlanes = require('./routes/planes');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/planes', indexPlanes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
