2796ffbd7c626bc9ecb12aa2816dfe049ba2a15a
Eldar Odzha <lopio.lololozka@gmail.com>
2025-11-30 16:42:50 +0300

    plane_8_1

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/plane2025')
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
