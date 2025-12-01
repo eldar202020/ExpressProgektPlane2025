743378e571c5c6f705ee356dd916a8399c41a1df
Eldar Odzha <lopio.lololozka@gmail.com>
2025-12-01 15:48:39 +0300

    plane_10_7

--- routes/index.js
+++ routes/index.js
@@ -31,9 +31,16 @@ router.post("/logreg", async function (req, res, next) {
       req.session.user_id = foundUser._id;
       res.redirect("/");
     } else {
-       res.render('logreg',{title: 'Вход', error: 'Пароль не верный'});
+      res.render("logreg", { title: "Вход", error: "Пароль не верный" });
     }
   }
+});
+/* POST logout. */
+
+router.post("/logout", function (req, res, next) {
+  req.session.destroy();
+  res.locals.user = null;
+  res.redirect("/");
 });
 
 module.exports = router;
