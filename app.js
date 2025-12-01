ff5fe3726faa9f393ff738407d3a297bdbd62256
Eldar Odzha <lopio.lololozka@gmail.com>
2025-12-01 11:52:13 +0300

    plane_9_5

--- app.js
+++ app.js
@@ -45,6 +45,8 @@ app.use(function (req, res, next) {
   req.session.counter = req.session.counter + 1 || 1;
   next();
 });
+app.use(require("./middlewares/createMenu.js"))
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/planes", indexPlanes);
---
+++ middlewares/createMenu.js
@@ -0,0 +1,11 @@
+var Plane = require("../models/plane").Plane;
+
+module.exports = async function (req, res, next) {
+  res.locals.nav = [];
+  var menu = await Plane.find(null, { _id: 0, title: 1, nick: 1 });
+  console.log(menu);
+  if (menu.length != 0) {
+    res.locals.nav = menu;
+  }
+  next();
+};
