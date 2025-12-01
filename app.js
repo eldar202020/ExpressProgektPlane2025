6d457aadcdf6dd4a70503a6dc11ed9018120d756
Eldar Odzha <lopio.lololozka@gmail.com>
2025-12-01 11:21:55 +0300

    plane_9_3

--- app.js
+++ app.js
@@ -1,73 +1,66 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/plane2025')
-var session = require("express-session")
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var MongoStore = require("connect-mongo").MongoStore;
+mongoose.connect("mongodb://localhost/plane2025");
+var session = require("express-session");
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var indexPlanes = require('./routes/planes');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var indexPlanes = require("./routes/planes");
 
 var app = express();
 
 // view engine setup
-app.engine('ejs',require('ejs-locals'));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine("ejs", require("ejs-locals"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  session({
+    secret: "ThreePlanes",
+    cookie: { maxAge: 60 * 1000 },
+    proxy: true,
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl:"mongodb://localhost/plane2025"}),
+  })
+);
 
-app.use(session({
-
- secret: "ThreePlanes",
-
- cookie:{maxAge:60*1000},
-
- proxy: true,
-
- resave: true,
-
- saveUninitialized: true
-
-}))
-
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/planes', indexPlanes);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/planes", indexPlanes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   //res.render('error');
-  res.render('error',{title: 'Three Cats'});
-
+  res.render("error", { title: "Three Cats" });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.cookie("greeting", "Hi!!!").render("index", { title: "Express" });
+  req.session.greeting = "Hi!!!";
+  res.render("index", { title: "Express" });
 });
 module.exports = router;
