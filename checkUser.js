c06fefd4d8dd42000b7268d7f4bd1987bc2e5339
Eldar Odzha <lopio.lololozka@gmail.com>
2025-12-01 14:08:15 +0300

    plane_10_2

---
+++ checkUser.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost/plane2025")
+
+var User = require("./models/user.js").User
+
+
+var first_user = new User({
+
+   username: "Vasya",
+
+   password: "qwerty"
+
+})
+
+
+first_user.save();
