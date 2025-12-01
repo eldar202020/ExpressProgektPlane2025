18421fd87b39ca96fb2d675199a113b5159d088e
Eldar Odzha <lopio.lololozka@gmail.com>
2025-12-01 10:13:44 +0300

    plane_8_4

--- routes/planes.js
+++ routes/planes.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var Plane = require('../models/plane').Plane;
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с cats');
-});
-
 router.get("/:nick", async function(req, res, next) {
 
    var planes = await Plane.find({nick: req.params.nick});
